bb84f7faddd2669aec478b43cb398c0d7ea976f8
wl02058381 <wl02058381@gmail.com>
2025-04-27 00:53:21 +0800

    測試build成功

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // 確保伺服器埠號正確
+    port: 5173,
   },
+  build: {
+    rollupOptions: {
+      external: ['#minurl','#minpath','#minproc'], // 確保外部依賴
+    },
+  }
 });
