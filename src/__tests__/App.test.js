062183632794b236ee33fe952f5635a6dd021fce
wl02058381 <wl02058381@gmail.com>
2025-04-27 01:32:02 +0800

    測試部屬

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; // 確保引入 jest-dom 擴展
 import App from '../App';
 
-test('renders without crashing', () => {
+test('renders without crashing Test', () => {
   render(<App />);
 });
