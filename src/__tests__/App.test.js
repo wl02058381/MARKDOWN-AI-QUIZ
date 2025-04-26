d9d025dd10e32ccdc149452185e09b83994c3684
wl02058381 <wl02058381@gmail.com>
2025-04-27 00:45:58 +0800

    刪除垃圾

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,8 +3,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; // 確保引入 jest-dom 擴展
 import App from '../App';
 
-configure({ adapter: new Adapter() });
-
 test('renders without crashing', () => {
   render(<App />);
 });
